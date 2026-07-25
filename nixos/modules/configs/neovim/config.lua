361e44d91ba7c254c5db17186850efc75c4e04da
Ash Ametrine <me@ashametrine.dev>
2026-07-25 22:19:43 +0100

    feat(neovim): use relative line numbers

--- nixos/modules/configs/neovim/config.lua
+++ nixos/modules/configs/neovim/config.lua
@@ -32,6 +32,7 @@ vim.opt.linebreak = true
 vim.opt.list = true
 vim.opt.magic = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.scrolloff = 5
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
